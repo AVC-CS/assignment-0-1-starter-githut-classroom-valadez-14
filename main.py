b2f296e4a9eceed1cc833c381da892f449a65437
valadez-14 <avaladez1@students.avc.edu>
2023-08-18 22:35:24 -0700

    SOURCE CONTROL

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def main():
     # make your code below
     # print('Hello World')
     ##############################
-
+  print('Hello World my name is Adan Valadez')
+  print('CS122: Programming and Algorithms in Python')
 
 if __name__ == '__main__':
     main()
